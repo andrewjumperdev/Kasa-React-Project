d39b5b78acc372cf085bf8ed94ca327828324a91
andrewjumperdev <andrew.jumper.ent@gmail.com>
2023-08-14 13:08:01 +0200

    Dropdown Dinamic

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -3,36 +3,41 @@ import Dropdown from "../../components/Dropdowns";
 import "./Apropos.scss";
 
 const Apropos = () => {
+
+  const aproposData = [
+    {
+      title: 'Fiabilité',
+      description: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    },
+    {
+      title: 'Respect',
+      description: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      title: 'Service',
+      description: `Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.`
+    },
+    {
+      title: 'Sécurité',
+      description: `La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.`
+    },
+  ]
+
+
   return (
     <div className="main-container">
       <div className="hero-container">
         <div className="img"></div>
       </div>
       <div className="dropdown-container">
-        <Dropdown
-          className="dropdown-description"
-          title="Fiabilité"
-          description={
-            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-          }
-        />
-        <Dropdown
-          className="dropdown-description"
-          title="Respect"
-          description={
-            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-          }
-        />
-        <Dropdown
-          className="dropdown-description"
-          title="Service"
-          description={`Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.`}
-        />
-        <Dropdown
-          className="dropdown-description"
-          title="Sécurité"
-          description={`La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.`}
-        />
+        {aproposData.map((item, i) => 
+                <Dropdown
+                className="dropdown-description"
+                key={i}
+                title={item.title}
+                description={item.description}
+              />
+        )}
       </div>
     </div>
   );
